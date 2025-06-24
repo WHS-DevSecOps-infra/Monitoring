9e275b4cf2d2e08482e4d9908bcf47a03244d59b
rnjsdbwlsqwer <rnjsdbwls2006@naver.com>
2025-06-25 01:28:42 +0900

    Revert "Merge pull request #9 from WHS-DevSecOps-infra/feat/securitylake-cloudwatch"
    This reverts commit 79aa657e0c1e79caf50279cb431f63c7e3188d81, reversing
    changes made to a89674cb9290028378ce6d020f2cd0d982ac42c2.

--- terraform-siem/modules/securitylake/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_security_lake" "this" {
-  enable_all = true
-}
-
-resource "aws_security_lake_organization_configuration" "org_config" {
-  auto_enable = {
-    all_regions = true
-  }
-}
