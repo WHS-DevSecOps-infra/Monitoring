974eb5d37475b8b2c1e3e5a11e8159ba706c4e1e
rnjsdbwlsqwer <rnjsdbwls2006@naver.com>
2025-06-25 01:29:35 +0900

    Revert "Merge pull request #8 from WHS-DevSecOps-infra/feat/#7"
    This reverts commit a89674cb9290028378ce6d020f2cd0d982ac42c2, reversing
    changes made to 90f427782fa545353e37077a7319cb89f3b0690c.

--- terraform-siem/modules/firehose/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "opensearch_domain_arn" {
-  type = string
-}
-
-variable "s3_bucket_arn" {
-  type = string
-}
--- terraform-siem/modules/opensearch/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "domain_arn" {
-  value = aws_opensearch_domain.siem.arn
-}
