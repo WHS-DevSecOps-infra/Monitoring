c2d789cfcd6b92b87e4887592dd630b11d7d449a
3olly <cy.kim@ewhain.net>
2025-07-07 11:02:24 +0900

    fix: s3 main.tf kms 정책 수정사항

--- management-team-account/outputs.tf
+++ management-team-account/outputs.tf
@@ -2,3 +2,11 @@ output "management_account_id" {
   description = "Account ID of the management account"
   value       = data.aws_caller_identity.current.account_id
 }
+
+output "debug_cloudtrail_s3_bucket_name" {
+  value = data.terraform_remote_state.operation.outputs.bucket_name
+}
+
+output "debug_cloudtrail_kms_key_arn" {
+  value = data.terraform_remote_state.operation.outputs.kms_key_arn
+}
