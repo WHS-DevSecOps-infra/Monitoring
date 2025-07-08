98e0d45c6ded29f0cc2281e22fe08561e1a6ff15
3olly <cy.kim@ewhain.net>
2025-07-08 09:51:24 +0900

    fix:cloudtrail->s3 로그 전송 성공

--- management-team-account/variables.tf
+++ management-team-account/variables.tf
@@ -1,31 +1,14 @@
 variable "aws_region" {
-  description = "AWS Region"
+  description = "AWS 리전"
   type        = string
-  default     = "ap-northeast-2"
 }
 
 variable "org_trail_name" {
-  description = "Name of the organization trail"
-  type        = string
-  default     = "org-cloudtrail"
-}
-
-variable "destination_s3_bucket_name" {
-  description = "S3 bucket name in operation account"
+  description = "Organization CloudTrail 이름"
   type        = string
 }
 
-variable "destination_s3_bucket_arn" {
-  description = "ARN of the S3 bucket in operation account"
-  type        = string
-}
-
-variable "s3_kms_key_arn" {
-  description = "ARN of the KMS key used to encrypt the logs (in operation account)"
-  type        = string
-}
-
-variable "sso_role_name" {
-  description = "The name of the AWS SSO role to attach policy to"
+variable "cloudtrail_bucket_name" {
+  description = "Operation 계정에서 생성된 CloudTrail 로그용 S3 버킷 이름"
   type        = string
 }
--- operation-team-account/modules/s3/outputs.tf
+++ operation-team-account/modules/s3/outputs.tf
@@ -1,14 +1,11 @@
 output "bucket_name" {
-  value       = aws_s3_bucket.cloudtrail_logs.bucket
-  description = "S3 bucket name for CloudTrail logs"
+  value = aws_s3_bucket.logs.bucket
 }
 
 output "bucket_arn" {
-  value       = aws_s3_bucket.cloudtrail_logs.arn
-  description = "ARN of the S3 bucket"
+  value = aws_s3_bucket.logs.arn
 }
 
 output "kms_key_arn" {
-  value       = aws_kms_key.cloudtrail.arn
-  description = "KMS key ARN for S3 encryption"
+  value = aws_kms_key.cloudtrail.arn
 }
