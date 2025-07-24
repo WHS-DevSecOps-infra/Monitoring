baf99b1c992b5bcdba4cbec274620e1267600ca5
Choi Yunho <cyunho62100@dgu.ac.kr>
2025-07-24 13:40:14 +0900

    Inspector 스캔을 통한 취약점 검사

---
+++ operation-team-account/runtime-verification/inspector/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+}
+
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_inspector2_enabler" "this" {
+  account_ids    = [data.aws_caller_identity.current.account_id]
+  resource_types = ["EC2"]
+}
---
+++ operation-team-account/runtime-verification/lambda/main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+}
+
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+data "terraform_remote_state" "iam" {
+  backend = "s3"
+  config = {
+    bucket         = "cloudfence-operation-state"
+    key            = "runtime-verification/iam.tfstate"
+    region         = "ap-northeast-2"
+    dynamodb_table = "s3-operation-lock"
+  }
+}
+
+resource "aws_lambda_function" "inspector_slack_notification" {
+    function_name = "inspector_slack_notification"
+    role          = data.terraform_remote_state.iam.outputs.lambda_exec_role_arn
+    handler       = "index.lambda_handler"
+    runtime       = "python3.9"
+
+    environment {
+        variables = {
+            SLACK_WEBHOOK_URL = var.slack_webhook_url
+        }
+    }
+    filename      = "lambda_function_payload.zip"
+    source_code_hash = filebase64sha256("lambda_function_payload.zip")
+}
