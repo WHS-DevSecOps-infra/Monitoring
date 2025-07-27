9c0fe2aa01c3e71654eec22d934c08a0f0be9dca
3olly <cy.kim@ewhain.net>
2025-07-27 19:49:11 +0900

    chore: output 수정

--- modules/opensearch_domain/outputs.tf
+++ modules/opensearch_domain/outputs.tf
@@ -2,3 +2,9 @@ output "endpoint" {
   value     = aws_opensearch_domain.this.endpoint
   sensitive = true
 }
+
+output "opensearch_domain_id" {
+  description = "The ID of the OpenSearch domain"
+  value       = aws_opensearch_domain.this.id
+  sensitive   = true
+}
